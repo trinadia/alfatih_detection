a73d1408ba71e324386b0c30b14cd297cfc23e47
Trinadia K <91521969+trinadia@users.noreply.github.com>
2024-04-11 22:09:46 +0700

    Create run.sh

---
+++ docker/run.sh
@@ -0,0 +1,12 @@
+# mkdir -p /path/to/weights
+# cp my-weights.pt /path/to/weights
+xhost +local:
+docker run -it --rm \
+           --runtime nvidia \
+           --network host \
+           --device /dev/video0:/dev/video0:mrw \
+           -e DISPLAY=$DISPLAY \
+           -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
+           -v /tmp/.X11-unix/:/tmp/.X11-unix \
+           -v /path/to/weights:/weights \
+           yolov5 python3.8 detect.py --source 0 --weights /weights/my-weights.pt
