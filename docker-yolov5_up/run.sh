ed03cbc009e696fa25ee14845bd02a143ca313c7
Trinadia K <91521969+trinadia@users.noreply.github.com>
2024-05-20 23:04:09 +0700

    Update run.sh

--- docker-yolov5_up/run.sh
+++ docker-yolov5_up/run.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-# export PYTHONPATH=/weights:$PYTHONPATH
 xhost +local:
-sudo docker run \
-       -it \
+/usr/bin/sudo /usr/bin/docker run \
+       -i \
        --rm \
        --runtime nvidia \
        --network host \
+       --device /dev/ttyACM0:/dev/ttyACM0 \
        --device /dev/video0:/dev/video0:mrw \
        -e DISPLAY=$DISPLAY \
        -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
        -v /tmp/.X11-unix/:/tmp/.X11-unix \
        -v /home/alfatih/docker-yolov5:/weights \
-       yolov5 python3.8 detect_display.py --source 0 --weights /weights/best_new_640.pt
+       yolov5 python3.8 detect_ACM0.py --source 0 --weights /weights/best_new_640.pt
